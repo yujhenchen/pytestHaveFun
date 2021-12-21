210de83b81e7296684e52eeaeb124e773aa88dc4
yujhenchen <yujhen.chen93@gmail.com>
2021-12-21 13:19:19 +0800

    form apu token into request header

--- tests/testCarbonInterfaceAPI/estimatesClient.py
+++ tests/testCarbonInterfaceAPI/estimatesClient.py
@@ -8,13 +8,19 @@ class EstimatesClient(object):
     def __init__(self):
         super().__init__()
 
+    def get_request_headers(apiKey):
+        return {'Authorization': "Bearer " + apiKey}
+
     def post_flight_estimates(apiToken, params):
 
         return
 
     def get_all_estimates(apiKey, *querieargs):
-        getRequest = requests.get(
-            url=EstimatesClient.url + EstimatesClient.path, headers={'Authorization': apiKey})
-        response = getRequest.json()
-        print(response)
-        return
+        response = requests.get(
+            url=EstimatesClient.url + EstimatesClient.path, headers=EstimatesClient.get_request_headers(apiKey))
+
+        print("status code: " + str(response.status_code))
+        # print("response headers: "+str(response.headers))
+        responseJson = response.json()
+        # print(responseJson)
+        return responseJson
