9027338d2590907d4f035c6dc21f73ab014bbee7
yujhenchen <yujhen.chen93@gmail.com>
2021-12-21 11:21:52 +0800

    add api test basic structure files

---
+++ tests/testCarbonInterfaceAPI/estimatesClient.py
@@ -0,0 +1,12 @@
+import requests
+
+
+class EstimatesClient(object):
+    def __init__(self):
+        super().__init__()
+
+    def post_flight_estimates():
+        return
+
+    def get_all_estimates():
+        return
---
+++ tests/testCarbonInterfaceAPI/testEstimates.py
@@ -0,0 +1,9 @@
+import pytest_check as check
+
+
+def test_post_flight_estimates():
+    return
+
+
+def test_get_all_estimates():
+    return
