9310bd4a2d62e46907fb8e961bc55d1602157e0e
yujhenchen <yujhen.chen93@gmail.com>
2021-12-24 14:55:16 +0800

    add parallel run package, add desc

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,8 @@ def pytest_addoption(parser):
                      default="userA", help="userA or userB")
     parser.addoption("--password", action="store",
                      default="00000", help="Please provide corresponding passwords")
+
+    # for api testing use
     parser.addoption("--apiKey", action="store",
                      default="00000", help="Please provide API key")
 
@@ -15,6 +17,8 @@ def params(request):
     params = {}
     params['user'] = request.config.getoption('--user')
     params['password'] = request.config.getoption('--password')
+
+    # for api testing use
     params['apiKey'] = request.config.getoption('--apiKey')
     # if params['user'] is None or params['password'] is None:
     #     pytest.skip()
