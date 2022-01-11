60d296c5ce26df934ca806255c163ea226449ad4
yujhenchen <yujhen.chen93@gmail.com>
2022-01-11 16:45:17 +0800

    fix module import by adding access to parent

--- handleModules/module_C/classC.py
+++ handleModules/module_C/classC.py
@@ -1,3 +1,19 @@
+import os
+import sys
+
+# source: https://www.geeksforgeeks.org/python-import-from-parent-directory/
+# getting the name of the directory
+# where the this file is present.
+current = os.path.dirname(os.path.realpath(__file__))
+
+# Getting the parent directory name
+# where the current directory is present.
+parent = os.path.dirname(current)
+
+# adding the parent directory to
+# the sys.path.
+sys.path.append(parent)
+
 from module_A.classA import A
 from module_B.classB import B
 
